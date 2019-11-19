505c9bc9603ff52e050ff70bc1df24278f82da07
FiveBreads <49057625+FiveBreads@users.noreply.github.com>
2019-11-19 10:43:45 +0900

    탑코더-탐욕법-다양한색상의 상자와 공

--- topCoder/src/greedy/ColorfulBoxesAndBalls.java
+++ topCoder/src/greedy/ColorfulBoxesAndBalls.java
@@ -2,11 +2,12 @@ package greedy;
 
 public class ColorfulBoxesAndBalls {
 	public int getMaximum(int numRed, int numBlue, int onlyRed, int onlyBlue, int bothColors) {
-		
-		
-		
-		
-		
-		return 0;
+		int n = Math.min(numBlue, numRed);
+		int ans = Integer.MIN_VALUE;
+		for(int i = 0; i < n; i++) {
+			int res = (numRed-i)*onlyRed + (numBlue-i)*onlyBlue + (i*2*bothColors);
+			ans = Math.max(res, ans);
+		}
+		return ans;
 	}
 }
